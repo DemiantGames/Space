5c3401ed9ea3f7a7bacd8efb7ba3a4f88e062d65
Demián <demiandal@gmail.com>
2016-04-19 18:57:48 -0500

    Scapace

---
+++ Assets/Prefabs/Explosion.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Explosion : MonoBehaviour {
+	// Use this for initialization
+	private FMOD.Studio.EventInstance explo;
+	void Start () {
+		explo = FMODUnity.RuntimeManager.CreateInstance ("event:/Planet Explosion");
+	}
+	
+
+
+	void OnDestroy() {
+		explo.start ();
+	}
+}
